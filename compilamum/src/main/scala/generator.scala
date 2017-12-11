16cdffff46e538125fe3a756eb39b08768917707
Lalaithion <izaak.weiss@gmail.com>
2017-12-10 23:31:17 -0700

    global variables now work!

--- compilamum/src/main/scala/generator.scala
+++ compilamum/src/main/scala/generator.scala
@@ -28,7 +28,8 @@ object Generate {
     }
     case Block(stmts) => stmts map { line => s"${gen(line)};" } mkString("\n")
     case Discard(stmt) => gen(stmt)
-    case Assign(x, v) => s"var $x = ${gen(v)}"
+    case Declare(x, _, v) => s"var $x = ${gen(v)}"
+    case Assign(x, v) => s"$x = ${gen(v)}"
     case Return(expr) => s"return ${gen(expr)}"
 
     case Call(f, args) => s"${f}(${args map gen mkString(", ")})"
